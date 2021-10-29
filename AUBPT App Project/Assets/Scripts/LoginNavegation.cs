fd8ffed5e6c3da593bed63b9d022848930672bdf
Matthew Murray <matthew.murray@duke.edu>
2021-10-29 10:53:13 -0400

    Added button functionalities to text

---
+++ AUBPT App Project/Assets/Scripts/LoginNavegation.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginNavegation : MonoBehaviour
+{
+
+//Create variables to tell which objects will be referenced
+public GameObject LoginPage;
+public GameObject ForgotPage;
+public GameObject ImPage;
+public GameObject SignUpParticipant;
+public GameObject SignUpClinician;
+    
+    
+    
+// default app startup
+void Start() //login page launches when user starts app
+{
+    LoginPage.SetActive(true);
+        
+}
+
+public void WhoIm(){ 
+
+    ImPage.SetActive(true);
+    LoginPage.SetActive(false);
+    ForgotPage.SetActive(false);
+    SignUpParticipant.SetActive(false);
+    SignUpClinician.SetActive(false);
+
+}
+
+public void CreateAccountClinician(){
+
+    SignUpClinician.SetActive(true);
+    ImPage.SetActive(false);
+    LoginPage.SetActive(false);
+    ForgotPage.SetActive(false);
+    SignUpParticipant.SetActive(false);
+    
+
+}
+
+public void CreateAccountParticipant(){
+
+    SignUpParticipant.SetActive(true);
+    SignUpClinician.SetActive(false);
+    ImPage.SetActive(false);
+    LoginPage.SetActive(false);
+    ForgotPage.SetActive(false);
+    
+
+}
+
+public void ForgotMyLogin(){
+
+    ForgotPage.SetActive(true);
+    SignUpParticipant.SetActive(false);
+    SignUpClinician.SetActive(false);
+    ImPage.SetActive(false);
+    LoginPage.SetActive(false);
+    
+
+}
+
+public void GoBackLoginPge(){
+    
+    LoginPage.SetActive(true);
+    ImPage.SetActive(false);    
+    ForgotPage.SetActive(false);
+    SignUpParticipant.SetActive(false);
+    SignUpClinician.SetActive(false);
+
+}
+
+public void OpenTaskPage(){
+      SceneManager.LoadScene("TaskListPage");
+  }
+
+}
