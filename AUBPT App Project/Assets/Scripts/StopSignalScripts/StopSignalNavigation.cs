614673656144f91dbc246f009b1cda72fde755c0
Karan Desai <karandesai@Karans-MBP.lan>
2021-11-30 20:23:51 -0500

    stop signal

--- AUBPT App Project/Assets/Scripts/StopSignalScripts/StopSignalNavigation.cs
+++ AUBPT App Project/Assets/Scripts/StopSignalScripts/StopSignalNavigation.cs
@@ -20,6 +20,7 @@ public GameObject TrialPanel1;
 public GameObject TrialPanel2;
 
 public GameObject CorrectResponsePanel;
+public GameObject IncorrectResponsePanel;
 public GameObject TrialBlockPanel;
 
 public AudioSource audioSource;
@@ -27,13 +28,20 @@ public float delay;
 
 private float waitTime;
 public int numberTrials = 0;
+public int totalTrials = 0;
 public bool trial;
 public int panel = 1;
 public float reactionTime;
 public float timeSinceStartup;
 
-public int correctResponse;
-public int incorrectResponse;
+//public int correctResponse;
+//public int incorrectResponse;
+public List<List<bool>> correctness = new List<List<bool>>();
+
+public bool result;
+public bool pressed;
+
+public string output;
 
 public List<float> listReactionTimes = new List<float>();
 
@@ -157,56 +165,94 @@ public void TrialPanelOne(){
 }
 
 
-// private void Start() {
-//     StartCoroutine(InfiniteLoop());
-// }
-// private IEnumerator InfiniteLoop()
-//     {
-//         waitTime = Random.Range(1, 3);
-//     while (true){
-//         # yield return new WaitForSeconds(waitTime);
-//         Panel6.SetActive(true);
-//         Panel5.SetActive(false);
-//         # yield return new WaitForSeconds(waitTime);
-//         Panel5.SetActive(true);
-//         Panel6.SetActive(false);
-//         }
-//
-//     }
+public void chooseNeither() {
+  if (pressed == false) {
+    List<bool> currCorrect = new List<bool>();
+    currCorrect.Add(true);
+    currCorrect.Add(result);
+    correctness.Add(currCorrect);
+    numberTrials++;
+    totalTrials++;
+    chooseTrue();
+  }
+}
+
+
 
 public void chooseRight() {
+  // If right or left button is true and result is true, then this is a wrong behavior
+  // Transform function into a variable
+
   reactionTime = Time.time - timeSinceStartup;
-  //Debug.Log(reactionTime);
+  // Debug.Log(reactionTime);
+  //
   listReactionTimes.Add(reactionTime);
 
-  if (panel == 0) {
-    //chooseTrue();
-    TrialAlternate(true);
-    correctResponse++;
+  pressed = true;
 
+  if (panel == 0) {
+    TrialAlternate(true);
   }
   else {
     TrialAlternate(false);
-    incorrectResponse++;
   }
+
+  // if (result == true) {
+  //   TrialAlternate(false);
+  // }
+
+  // if (result == false) {
+  //   if (panel == 0) {
+  //     //chooseTrue();
+  //     TrialAlternate(true);
+  //     correctResponse++;
+  //
+  //   }
+  //
+  //   else {
+  //     TrialAlternate(false);
+  //     incorrectResponse++;
+  //   }
+  // }
+
 
 }
 
 public void chooseLeft() {
   reactionTime = Time.time - timeSinceStartup;
-  //Debug.Log(reactionTime);
+  // Debug.Log(reactionTime);
+
+  // Differentiate between right and wrong reaction time
+  // Maybe a dictionary to store correct and incorrect responses
+  // Key is the panel number and value is whether response is True or False
+  // String correct or wrong
+
   listReactionTimes.Add(reactionTime);
+  pressed = true;
 
   if (panel == 1) {
-    //chooseTrue();
     TrialAlternate(true);
-    correctResponse++;
-
   }
   else {
     TrialAlternate(false);
-    incorrectResponse++;
   }
+
+  // if (result == true) {
+  //   TrialAlternate(false);
+  // }
+  //
+  // if (result == false) {
+  //   if (panel == 1) {
+  //     //chooseTrue();
+  //     TrialAlternate(true);
+  //     correctResponse++;
+  //
+  //   }
+  //   else {
+  //     TrialAlternate(false);
+  //     incorrectResponse++;
+  //   }
+  // }
 
 
 }
@@ -223,14 +269,28 @@ public void chooseTrue() {
     TrialPanel2.SetActive(!trial);
     WhistleAudio();
   }
+}
+
+public void chooseFalse() {
+  StartCoroutine(BadJob());
+
+  IEnumerator BadJob() {
+    yield return new WaitForSeconds(0);
+    IncorrectResponsePanel.SetActive(true);
+    yield return new WaitForSeconds((float)0.5);
+    IncorrectResponsePanel.SetActive(false);
+    TrialPanel1.SetActive(trial);
+    TrialPanel2.SetActive(!trial);
+    WhistleAudio();
+  }
 
 
 }
 
-public void displayGoobJobBlock() {
-  StartCoroutine(GoodJob());
+public void endOfBlock() {
+  StartCoroutine(endBlock());
 
-  IEnumerator GoodJob() {
+  IEnumerator endBlock() {
     TrialPanel1.SetActive(false);
     TrialPanel2.SetActive(false);
     yield return new WaitForSeconds(0);
@@ -239,73 +299,168 @@ public void displayGoobJobBlock() {
     CorrectResponsePanel.SetActive(false);
     TrialBlockPanel.SetActive(true);
   }
+}
+
+public void endOfBlockFalse() {
+  StartCoroutine(endBlock());
+
+  IEnumerator endBlock() {
+    TrialPanel1.SetActive(false);
+    TrialPanel2.SetActive(false);
+    yield return new WaitForSeconds(0);
+    IncorrectResponsePanel.SetActive(true);
+    yield return new WaitForSeconds(1);
+    IncorrectResponsePanel.SetActive(false);
+    TrialBlockPanel.SetActive(true);
+  }
 
 
 }
 
 public void TrialAlternate(bool dir) {
 
+  List<bool> currCorrect = new List<bool>();
 
-    if (numberTrials <= 3) {
-      trial =  Random.Range(0f, 1f) > 0.50;
-      panel = trial ? 1 : 0;
-      numberTrials++;
+  if (numberTrials <= 2) {
+    trial =  Random.Range(0f, 1f) > 0.50;
+    panel = trial ? 1 : 0;
+    numberTrials++;
+    totalTrials++;
 
-      // reactionTime = Time.time;
-
-      if (dir == true) {
-        // yield return new WaitForSeconds(2);
-        chooseTrue();
-        // TrialPanel1.SetActive(trial);
-        // TrialPanel2.SetActive(!trial);
-        timeSinceStartup = Time.time + 1;
-      }
-
-      if (dir == false) {
-
-        TrialPanel1.SetActive(trial);
-        TrialPanel2.SetActive(!trial);
-        WhistleAudio();
-        timeSinceStartup = Time.time;
-
-        //yield return new WaitForSeconds(2);
-        //CorrectResponsePanel.SetActive(true);
-      }
-
-
+    if (dir == true && result == false)  {
+      timeSinceStartup = Time.time;
+      currCorrect.Add(dir);
+      currCorrect.Add(result);
+      correctness.Add(currCorrect);
+      chooseTrue();
+      //TrialPanel1.SetActive(trial);
+      //TrialPanel2.SetActive(!trial);
+      //WhistleAudio();
+    }
+    if (dir == false && result == false)  {
+      timeSinceStartup = Time.time;
+      currCorrect.Add(dir);
+      currCorrect.Add(result);
+      correctness.Add(currCorrect);
+      chooseFalse();
+      //TrialPanel1.SetActive(trial);
+      //TrialPanel2.SetActive(!trial);
+      //WhistleAudio();
+    }
+    if (result == true && pressed == true)  {
+      //timeSinceStartup = Time.time;
+      currCorrect.Add(false);
+      currCorrect.Add(result);
+      correctness.Add(currCorrect);
+      chooseFalse();
+      //TrialPanel1.SetActive(trial);
+      //TrialPanel2.SetActive(!trial);
+      //WhistleAudio();
+    }
+    if (result == true && pressed == false)  {
+      //timeSinceStartup = Time.time;
+      currCorrect.Add(true);
+      currCorrect.Add(result);
+      correctness.Add(currCorrect);
+      chooseTrue();
+      //TrialPanel1.SetActive(trial);
+      //TrialPanel2.SetActive(!trial);
     }
 
+  }
+
+  else {
+    // string result = "List contents: ";
+    // foreach (var item in listReactionTimes)
+    // {
+    //   result += item.ToString() + ", ";
+    // }
+    // Debug.Log(result);
+    //Debug.Log(incorrectResponse);
+
+    if (dir == true && result == false) {
+        endOfBlock();
+        currCorrect.Add(dir);
+        currCorrect.Add(result);
+        correctness.Add(currCorrect);
+    }
+    else if (result == true && pressed == false) {
+        endOfBlock();
+        currCorrect.Add(true);
+        currCorrect.Add(result);
+        correctness.Add(currCorrect);
+    }
+    else if (dir == false && result == false) {
+        endOfBlockFalse();
+        currCorrect.Add(dir);
+        currCorrect.Add(result);
+        correctness.Add(currCorrect);
+    }
+    else if (result == true && pressed == true) {
+        currCorrect.Add(false);
+        currCorrect.Add(result);
+        correctness.Add(currCorrect);
+        endOfBlockFalse();
+    }
     else {
-      // string result = "List contents: ";
-      // foreach (var item in listReactionTimes)
-      // {
-      //   result += item.ToString() + ", ";
-      // }
-      //Debug.Log(result);
-      //Debug.Log(incorrectResponse);
-
-      if (dir == true) {
-          displayGoobJobBlock();
-      }
-      else {
-        TrialPanel1.SetActive(false);
-        TrialPanel2.SetActive(false);
-        TrialBlockPanel.SetActive(true);
-      }
-
-      numberTrials = 0;
-      panel = 1;
+      TrialPanel1.SetActive(false);
+      TrialPanel2.SetActive(false);
+      TrialBlockPanel.SetActive(true);
     }
-    //Debug.Log(reactionTime);
+
+    string output = "List contents: ";
+    foreach (var item in correctness)
+    {
+      string combinedString = string.Join( ",", item);
+      output += combinedString + ", ";
+    }
+    Debug.Log(output);
+    //Debug.Log(correctness);
+    numberTrials = 0;
+    panel = 1;
+  }
+
+
+
+
 }
+
+
+//     }
+// //
+//     else {
+//       // string result = "List contents: ";
+//       // foreach (var item in listReactionTimes)
+//       // {
+//       //   result += item.ToString() + ", ";
+//       // }
+//       //Debug.Log(result);
+//       //Debug.Log(incorrectResponse);
+//
+//       if (dir == true) {
+//           endOfBlock();
+//       }
+//       else {
+//         TrialPanel1.SetActive(false);
+//         TrialPanel2.SetActive(false);
+//         TrialBlockPanel.SetActive(true);
+//       }
+//
+//       numberTrials = 0;
+//       panel = 1;
+//     }
+//     //Debug.Log(reactionTime);
+// }
 
 
 public void WhistleAudio() {
 
-  bool result = Random.Range(0f, 1f) > 0.75; //%25 percent chance to be true
+  result = Random.Range(0f, 1f) > 0.75; //%25 percent chance to be true
+
   System.Random random = new System.Random();
   double val = (random.NextDouble() * (0.234 - 0.069) + 0.069);
   delay = (float)val;
+  // If clicked, it is wrong - if not clicked, it is right
 
   if (result) {
     audioSource.PlayDelayed(delay);
@@ -314,11 +469,16 @@ public void WhistleAudio() {
   }
 
   IEnumerator StopSignalDelay() {
+    pressed = false;
     yield return new WaitForSeconds(3);
-    TrialAlternate(true);
+    chooseNeither();
+    //TrialAlternate(true);
+    //correctness.Add(totalTrials, dir, result);
+    //chooseTrue();
+    //TrialAlternate(true);
   }
 
-  Debug.Log(delay);
+  // Debug.Log(delay);
 }
 
 
