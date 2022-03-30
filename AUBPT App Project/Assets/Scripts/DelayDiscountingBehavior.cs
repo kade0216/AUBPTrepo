49d9351dd0fb743e81d835baa0b9489e4a4adba0
brendanskelleher <77758356+brendanskelleher@users.noreply.github.com>
2022-03-30 15:35:22 -0400

    DD Updates pt 2
    Added 3 min timer that appears in final minute → resets task if counts down to 0
    Added point on instructions about avg length of task (2 mins)
    Added extra instructions page detailing how long they have for each choice, as well as information on how the countdown timer will look and behave

--- AUBPT App Project/Assets/Scripts/DelayDiscountingBehavior.cs
+++ AUBPT App Project/Assets/Scripts/DelayDiscountingBehavior.cs
@@ -9,11 +9,14 @@ public class DelayDiscountingBehavior : MonoBehaviour
     //panel variables
     public GameObject WelcomePanel;
     public GameObject InstructionsPanel;
+    public GameObject InstructionsTimerPanel;
     public GameObject GamePanelChoosing;
     public GameObject GamePanelResting;
     public GameObject EndPanel;
+    public GameObject AltEndPanel;
     public Text LeftText;
     public Text RightText;
+    public Text TimerText;
 
     //game variables
     public int adj_amount = 250;
@@ -23,11 +26,12 @@ public class DelayDiscountingBehavior : MonoBehaviour
     public int delayed_amount = 1000;//fixed for now
     public bool random;
     public int imm_side; //0 == left, 1 == right
-    //^^randomize this, either 0 or 1
     public int user_selection;//0 == left, 1 == right
+    public float start_time;
+    public bool on_choice = false;
 
     //exported user data variables
-    public float[] indifference_points = {1000,1000,1000,1000,1000,1000,1000};
+    public int[] indifference_points = new int[7];
     /*delay var + indiff index meanings:
     0 == 1 day
     1 == 1 week
@@ -50,12 +54,19 @@ public class DelayDiscountingBehavior : MonoBehaviour
     {
         WelcomePanel.SetActive(true);
         InstructionsPanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
         GamePanelChoosing.SetActive(false);
         GamePanelResting.SetActive(false);
         EndPanel.SetActive(false);
+        AltEndPanel.SetActive(false);
+
         random = Random.Range(0f, 1f) > 0.50;
         imm_side = random ? 0 : 1;
         UpdateText();
+
+        for(int a = 0; a < 7; a++){
+            indifference_points[a] = 1000;
+        }
     }
 
     public void OpenTaskPage(){
@@ -65,37 +76,70 @@ public class DelayDiscountingBehavior : MonoBehaviour
     public void OpenInstructionsPanel(){
         InstructionsPanel.SetActive(true);
         WelcomePanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
         GamePanelChoosing.SetActive(false);
         GamePanelResting.SetActive(false);
         EndPanel.SetActive(false);
+        AltEndPanel.SetActive(false);
+    }
+
+    public void OpenInstructionsTimerPanel(){
+        InstructionsTimerPanel.SetActive(true);
+        WelcomePanel.SetActive(false);
+        InstructionsPanel.SetActive(false);
+        GamePanelChoosing.SetActive(false);
+        GamePanelResting.SetActive(false);
+        EndPanel.SetActive(false);
+        AltEndPanel.SetActive(false);
     }
 
     public void OpenGamePanelChoosing(){
         GamePanelChoosing.SetActive(true);
         WelcomePanel.SetActive(false);
         InstructionsPanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
         GamePanelResting.SetActive(false);
         EndPanel.SetActive(false);
+        AltEndPanel.SetActive(false);
+        
+        on_choice = true;
+        start_time = Time.time;
     }
 
     public void OpenGamePanelResting(){
         GamePanelResting.SetActive(true);
         WelcomePanel.SetActive(false);
         InstructionsPanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
         GamePanelChoosing.SetActive(false);
         EndPanel.SetActive(false);
+        AltEndPanel.SetActive(false);
     }
 
     public void OpenEndPanel(){
         EndPanel.SetActive(true);
         WelcomePanel.SetActive(false);
         InstructionsPanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
         GamePanelChoosing.SetActive(false);
         GamePanelResting.SetActive(false);
+        AltEndPanel.SetActive(false);
+    }
+
+    public void OpenAltEndPanel(){
+        AltEndPanel.SetActive(true);
+        WelcomePanel.SetActive(false);
+        InstructionsPanel.SetActive(false);
+        InstructionsTimerPanel.SetActive(false);
+        GamePanelChoosing.SetActive(false);
+        GamePanelResting.SetActive(false);
+        EndPanel.SetActive(false);
     }
 
     public void LoadNextTrial(){
         OpenGamePanelResting();
+        on_choice = false;
+        TimerText.text = "";
         trial ++;
         if(user_selection == imm_side){
             indifference_points[delay] = imm_amount;
@@ -121,8 +165,6 @@ public class DelayDiscountingBehavior : MonoBehaviour
         }
         random = Random.Range(0f, 1f) > 0.50;
         imm_side = random ? 0 : 1;
-        //for boolean values: Random.Range(0f, 1f) > 0.50;
-        //for int: Random.Range(0, 1); do i have to cast as int?
         UpdateText();
         StartCoroutine(RestPanelDelay());
         IEnumerator RestPanelDelay(){
@@ -144,13 +186,13 @@ public class DelayDiscountingBehavior : MonoBehaviour
     public void UpdateText(){
         string imm_text = "$" + imm_amount.ToString() + " NOW";
         string delayed_text = "$" + delayed_amount.ToString() + " in ";
-        if(delay == 0) { delayed_text += "1 DAY"; }
-        else if(delay == 1) { delayed_text += "1 WEEK"; }
-        else if(delay == 2) { delayed_text += "1 MONTH"; }
-        else if(delay == 3) { delayed_text += "6 MONTHS"; }
-        else if(delay == 4) { delayed_text += "1 YEAR"; }
-        else if(delay == 5) { delayed_text += "5 YEARS"; }
-        else { delayed_text += "25 YEARS"; }
+        if(delay == 0){ delayed_text += "1 DAY"; }
+        else if(delay == 1){ delayed_text += "1 WEEK"; }
+        else if(delay == 2){ delayed_text += "1 MONTH"; }
+        else if(delay == 3){ delayed_text += "6 MONTHS"; }
+        else if(delay == 4){ delayed_text += "1 YEAR"; }
+        else if(delay == 5){ delayed_text += "5 YEARS"; }
+        else{ delayed_text += "25 YEARS"; }
         if(imm_side == 1){
             LeftText.text = delayed_text;
             RightText.text = imm_text;
@@ -164,6 +206,14 @@ public class DelayDiscountingBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int time_left = (int)(180.0 - (Time.time - start_time));
+        if(on_choice && time_left <= 60){
+            TimerText.text = time_left.ToString();
+            if(time_left <= 0){
+                on_choice = false;
+                TimerText.text = "";
+                OpenAltEndPanel();
+            }
+        }
     }
 }
